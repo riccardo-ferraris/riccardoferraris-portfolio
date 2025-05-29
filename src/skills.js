7ac5cd34201cd877c584012831d114ca9dbf1ce0
RIckyfer02 <rickyferattack@gmail.com>
2025-05-29 18:02:54 +0200

    Aggiunto PL/SQL nelle skill

--- src/skills.js
+++ src/skills.js
@@ -1,4 +1,28 @@
-export const frontend = ["JavaScript", "React", "Flutter", "HTML", "CSS", "Tailwind CSS", "Bootstrap", "UI/UX Basics", "Responsive Design"];
-export const backend = ["Java", "Dart", "C#", "MySQL", "Python", "Firebase"];
+export const frontend = [
+  "JavaScript",
+  "React",
+  "Flutter",
+  "HTML",
+  "CSS",
+  "Tailwind CSS",
+  "Bootstrap",
+  "UI/UX Basics",
+  "Responsive Design",
+];
+export const backend = [
+  "Java",
+  "Dart",
+  "C#",
+  "MySQL",
+  "Python",
+  "Firebase",
+  "PL/SQL",
+];
 export const tools = ["Git", "GitHub", "Visual Studio Code"];
-export const soft = ["Problem Solving", "Teamwork", "Communication", "Adaptability", "Time Management"];
+export const soft = [
+  "Problem Solving",
+  "Teamwork",
+  "Communication",
+  "Adaptability",
+  "Time Management",
+];
