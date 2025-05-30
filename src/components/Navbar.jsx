3c0159687a6de1e7e0db15ba4d19b58500c43b2f
RIckyfer02 <rickyferattack@gmail.com>
2025-05-30 14:52:30 +0200

    Reso logo più piccolo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = ({ menuOpen, setMenuOpen }) => {
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <a href="#home" className="font-mono text-xl font-bold text-white">
-            <img src={logo} className="w-50 h-auto" alt="Logo" />
+            <img src={logo} className="w-45 h-auto" alt="Logo" />
           </a>
 
           <div
